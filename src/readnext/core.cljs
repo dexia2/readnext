84801293c2f071c05ad9e9864ce120ae562a161b
dexia2 <lindan.k2012.af.nk@gmail.com>
2018-01-11 11:30:55 +0900

    change select color

--- src/readnext/core.cljs
+++ src/readnext/core.cljs
@@ -226,9 +226,34 @@
         (play!)
         (println @play-context)))))
 
-(defn draw []
-  (q/stroke 0)
-  (q/fill 255)
+(defn setup []
+  (q/frame-rate 5))
+
+(def target-radius 15)
+(def sideline-width 20)
+(def court-width 300)
+(def court-height 300)
+(def target-padding 15)
+(def npc-targets #{
+                  {:direction :front-left :x 40 :y 110 }
+                  {:direction :front-right :x 240 :y 110 }
+                  {:direction :middle-left :x 40 :y 70 }
+                  {:direction :middle-right :x 240 :y 70 }
+                  {:direction :back-left :x 40 :y 30 }
+                  {:direction :back-right :x 240 :y 30 }
+                  })
+
+;計算で出したいが、関係性を記述するのがしんどい
+(def pc-targets #{
+                  {:direction :front-left :x 40 :y 180 }
+                  {:direction :front-right :x 240 :y 180 }
+                  {:direction :middle-left :x 40 :y 220 }
+                  {:direction :middle-right :x 240 :y 220 }
+                  {:direction :back-left :x 40 :y 260 }
+                  {:direction :back-right :x 240 :y 260 }
+                  })
+
+(defn draw-court []
   (q/rect 0 0 290 290)                                      ;コート全体
   (q/line 20 0 20 290)                                      ;サイドライン（左)
   (q/line 145 0 145 290)                                    ;センターライン（縦）
@@ -236,23 +261,45 @@
   (q/line 0 50 290 50)                                      ;サービスライン（ダブルス）
   (q/line 0 145 290 145)                                    ;ネット
   (q/line 0 240 290 240)                                    ;サービスライン（ダブルス）
-  (q/ellipse 40 30 30 30)                                   ;相手コート奥（左）ターゲット
-  (q/ellipse 240 30 30 30)                                  ;相手コート奥（右）ターゲット
-  (q/ellipse 40 70 30 30)                                   ;相手コート横（左）ターゲット
-  (q/ellipse 240 70 30 30)                                  ;相手コート横（右）ターゲット
-  (q/ellipse 40 110 30 30)                                   ;相手コート前（左）ターゲット
-  (q/ellipse 240 110 30 30)                                  ;相手コート前（右）ターゲット
+  )
+
+(defn draw-npc-targets []
+  (doseq [{x :x y :y} (seq npc-targets)]
+    (q/ellipse x y  (* target-radius 2) (* target-radius 2))))
+
+(defn draw-pc-targets [coll]
+  (doseq [{x :x y :y} coll]
+    (q/ellipse x y  (* target-radius 2) (* target-radius 2))))
+
+;たぶんマウスターゲットをもらって、それと比較するのが正しい
+;そして、たぶん色を変えるものを後ろにもっていくようにソートすればいい
+;あるいは色とそれ以外に分割する
+;そうすれば、最後だけ色を変えて描画すればよい。
+;上の関数は抽象化すべし
+(defn in-target [mouse-x mouse-y]
+  (filter (fn [{x :x y :y} target] (and (<= (- x target-radius)
+                                            mouse-x
+                                            (+ x target-radius))
+                                        (<= (- y target-radius)
+                                            mouse-y
+                                            (+ y target-radius))))
+  (seq pc-targets)))
+
+(defn draw []
+  (q/stroke 0)
+  (q/fill 255)
+  (draw-court)
   (q/stroke 240 179 37)
-  (q/fill 240 179 37)
-  (q/ellipse 40 180 30 30)                                   ;自分コート前（左）ターゲット
-  (q/ellipse 240 180 30 30)                                  ;自分コート前（右）ターゲット
-  (q/ellipse 40 220 30 30)                                   ;自分コート横（左）ターゲット
-  (q/ellipse 240 220 30 30)                                  ;自分コート横（右）ターゲット
-  (q/ellipse 40 260 30 30)                                   ;自分コート奥（左）ターゲット
-  (q/ellipse 240 260 30 30)                                  ;自分コート奥（右）ターゲット
+  (draw-npc-targets)
+
+  (draw-pc-targets (seq pc-targets))
+  (let [in (in-target (q/mouse-x) (q/mouse-y))]
+      (q/fill 240 179 37)
+      (draw-pc-targets in))
   )
 
 (q/defsketch court
+             :setup setup
              :draw draw
              :host "court"
              :size [300 300])
